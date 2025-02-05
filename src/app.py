e74b6be9a6727ddc5251f5f3b5fba362cb67a749
rubyyii <rubyyii65@gmail.com>
2025-02-05 21:48:12 +0100

    Muestro productos en el index, con error si intenta añadirlo sin haber logueado.

--- src/app.py
+++ src/app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, request
+from flask_mysqldb import MySQL
+from config import config
 
 app = Flask(__name__)
+# Inicio MySQL en mi applicacion.
+mysql = MySQL(app)
 
 #INDEX
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM products')
+        products = cur.fetchall()
 
-
-
-    return render_template('index.html')
+        return render_template('index.html', products=products)
+    
+    if request.method == 'POST':
+        flash('Tienes que registrarte antes de añadir un producto a favoritos.')
+        return redirect(url_for('index'))
 
 #REGISTER
 @app.route('/register', methods=['GET', 'POST'])
@@ -27,7 +37,6 @@ def login():
 def users():
 
     return render_template('users.html')
-
 
 
 
---
+++ src/db/products.sql
@@ -0,0 +1,5 @@
+USE flask_practice_sql;
+
+INSERT INTO products VALUES(NULL, 'Monitor', 199.99, 'https://misura.s11.cdn-upgates.com/_cache/1/d/1d0069eb810fe736492c446f78e2c93d-qm24dfi-foto01.jpg'),
+(NULL, 'Raton', 119.99, 'https://thumb.pccomponentes.com/w-530-530/articles/1077/10773108/1420-logitech-g-pro-x-superlight-2-lightspeed-raton-inalambrico-gaming-rosa-44000-dpi-58d8df10-8109-4771-870f-e041143d2e5c.jpg'),
+(NULL, 'Teclado', 189.99, 'https://assets2.razerzone.com/images/pnx.assets/1a0339cc1621d3b23d7e79658bea2c4a/razer-huntsman-v3-pro-tkl-line_500x500-v2.webp')
